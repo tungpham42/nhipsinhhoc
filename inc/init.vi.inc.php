7a2aad4680041ede33db4353eb910276d9092f02
Tung Pham <tung.42@gmail.com>
2019-12-13 07:59:03 +0700

    Update init.vi.inc.php

--- inc/init.vi.inc.php
+++ inc/init.vi.inc.php
@@ -3,12 +3,12 @@
 //ini_set('session.save_path','/tmp');
 //ini_set('session.save_handler', 'redis');
 //ini_set('session.save_path', 'tcp://127.0.0.1:6379');
-//require realpath($_SERVER['DOCUMENT_ROOT']).'/inc/compress.inc.php';<?php
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") {
-	$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: ' . $redirect);
-	exit();
+//require realpath($_SERVER['DOCUMENT_ROOT']).'/inc/compress.inc.php';
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
 }
 require realpath($_SERVER['DOCUMENT_ROOT']).'/inc/pusher.inc.php';
 require realpath($_SERVER['DOCUMENT_ROOT']).'/inc/functions.vi.inc.php';
